9b069fc142b7bd4e639db2c1aef6ae3f733f1ed5
CarloBottini <carlo.bottinimorcillo@usp.ceu.es>
2025-04-10 12:01:25 +0200

    doctormanager

--- src/OrganClinicJDBC/JDBCDoctorManager.java
+++ src/OrganClinicJDBC/JDBCDoctorManager.java
@@ -1,5 +1,65 @@
 package OrganClinicJDBC;
 
-public class JDBCDoctorManager {
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
+import OrganClinicINTERFACEs.DoctorManager;
+import OrganClinicPOJOs.Doctor;
+
+
+
+
+public class JDBCDoctorManager implements DoctorManager{
+	public JDBCManager manager;
+
+	public JDBCDoctorManager (JDBCManager m) {
+		this.manager=m;
+	}
+
+	
+
+	public List<Doctor> getAllDoctors(){
+		List<Doctor> doctors = new ArrayList<Doctor>();
+
+		try {
+			Statement stmt = manager.getConnection().createStatement();
+			String sql = "Select * FROM doctors";
+			ResultSet rs = stmt.executeQuery(sql);
+			while(rs.next()){
+				Integer id = rs.getInt("id");
+				String name= rs.getString("name");
+				Date dob= rs.getDate("date");
+				String gender = rs.getString("gender");
+				String email = rs.getString("email");
+				Integer telephone = rs.getInt("telephone");
+				
+				Doctor doctor= new Doctor(id,name,dob,gender,email,telephone);
+				doctors.add(doctor);
+				
+			
+			}
+
+			rs.close();
+			stmt.close();
+				
+		}catch(Exception e)
+		{e.printStackTrace();}
+		
+		return doctors;
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
