ece9cbc9878b24c806eb93ae66bb605af3bc0644
CarloBottini <carlo.bottinimorcillo@usp.ceu.es>
2025-05-01 14:20:23 +0200

    Blood POJO done

--- src/OrganClinicPOJOs/Blood.java
+++ src/OrganClinicPOJOs/Blood.java
@@ -1,10 +1,58 @@
 package OrganClinicPOJOs;
 
-public class Blood {
+import java.io.Serializable;
+import java.util.Objects;
 
+public class Blood implements Serializable{
+
+
+/*
+ * WE CAN DO THAT WHEN THE THEY CHOOSE THE BLOOD WECAN DO THAT THEY SELECT THEY BLOOD WITH A SELECTION
+ * */
+	
+	private static final long serialVersionUID = 1L;
+	private String type;
+	
+	public Blood(String type) {
+		super();
+		this.type=type;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(type);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Blood other = (Blood) obj;
+		return Objects.equals(type, other.type);
+	}
+
+	@Override
+	public String toString() {
+		return "Blood [type=" + type + "]";
+	}
+	
+	
+	
+	
 }
 
 /*
- * TODO GETTER, SETTER, COSNTRUCTOR VARIBALE TYPE
  * WE CAN DO THAT WHEN THE THEY CHOOSE THE BLOOD WECAN DO THAT THEY SELECT THEY BLOOD WITH A SELECTION
  * */
