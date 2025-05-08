2bfb2e2caa380c2bb143d8a286151c9b998f5697
elisaMelgosa <elisa.melgosamartin@usp.ceu.es>
2025-05-08 17:38:51 +0200

    nurse

--- src/OrganClinicJDBC/JDBCNurseManager.java
+++ src/OrganClinicJDBC/JDBCNurseManager.java
@@ -1,7 +1,96 @@
 package OrganClinicJDBC;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import OrganClinicPOJOs.Nurse;
+import OrganClinicPOJOs.Operation;
+import OrganClinicPOJOs.Patient;
+import OrganClinicPOJOs.Treatment;
+
 public class JDBCNurseManager {
-	
+private JDBCManager manager;
+
+	private JDBCNurseManager(JDBCManager m) {
+		this.manager = m;
+	}
+	public Nurse getNurseByID(Integer id) {
+		try {
+			String sql = "SELECT * FROM Nurse WHERE id = " + id;
+			Statement st = manager.getConnection().createStatement();
+			ResultSet rs= st.executeQuery(sql);
+			rs.next();
+			Nurse n = new Nurse (rs.getInt("id"), rs.getString("name"), rs.getBoolean("availability"));
+			return n;
+		}catch(Exception e) {
+			System.out.println("Error in the database");
+			e.printStackTrace();
+		}
+		return null;
+	}
+	public void deleteNurse(Integer id) {
+		try {
+			String st= "DELETE FROM Nurse WHERE id= ?";
+			PreparedStatement prep = manager.getConnection().prepareStatement(st);
+			prep.setInt(1, id);
+			int rowsAffected = prep.executeUpdate();
+			if (rowsAffected>0) {
+		        System.out.println("Deleted successfully the Nurse with ID"+ id);
+			}else {
+		        System.out.println("Nurse not found with ID " + id);
+			}
+			prep.close();		
+		}catch(Exception e) {
+			System.out.println("Error in data bases with the Nurse ID " + id);
+			e.printStackTrace();
+		}
+	}
+	public void addNurse(Nurse n) {
+		try {//it is not necessary to put the id, because it is autoincremented
+			String sql = "INSERT INTO Nurse (name,availability) VALUES (?,?)";
+
+			PreparedStatement prep = manager.getConnection().prepareStatement(sql);
+			prep.setString(1, n.getName());	
+			prep.setBoolean(2, n.getAvailability());
+		
+		
+						
+			prep.executeUpdate();
+			prep.close();
+		
+		}catch(Exception e){
+			System.out.println("Error in the database");
+			e.printStackTrace();
+		}	
+	}
+	public List<Nurse> getAllNurses(){
+		List<Nurse> allNurse = new ArrayList<Nurse>();
+		try {
+			Statement stmt = manager.getConnection().createStatement();
+			String sql = "Select * FROM Nurse";
+			ResultSet rs = stmt.executeQuery(sql);
+			
+			while(rs.next())
+			{
+				Integer id = rs.getInt("id");
+				String name= rs.getString("name");
+				Boolean availability = rs.getBoolean("availability");
+				
+				
+				Nurse n= new Nurse(id,name,availability);
+				allNurse.add(n);
+			}
+			rs.close();
+			stmt.close();
+		}catch(Exception e){
+			e.printStackTrace();
+			}
+		return allNurse;
+		}
+		
 	//TODO remember that now there is a list of operation
 
 }
