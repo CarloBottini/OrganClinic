10b1e9219cfa7f35bc110f21a64703ee41502af7
CarloBottini <carlo.bottinimorcillo@usp.ceu.es>
2025-05-08 13:28:24 +0200

    more aclarations about blood+ JDBCBlood

--- src/OrganClinicINTERFACEs/BloodManager.java
+++ src/OrganClinicINTERFACEs/BloodManager.java
@@ -2,4 +2,8 @@ package OrganClinicINTERFACEs;
 
 public interface BloodManager {
 
+
+	
 }
+
+//For the moment there are no methods for blood, we do not need to add or delete or modify any blood
--- src/OrganClinicJDBC/JDBCBloodManager.java
+++ src/OrganClinicJDBC/JDBCBloodManager.java
@@ -1,6 +1,14 @@
 package OrganClinicJDBC;
 
+import OrganClinicINTERFACEs.BloodManager;
 
-public class JDBCBloodManager {
+public class JDBCBloodManager implements BloodManager{
 
+	private JDBCManager manager;
+	
+	private JDBCBloodManager(JDBCManager m) {
+		this.manager=m;
+	}
+	
+	//We will not need to add/delete/modify any blood because it all the existent types of blood
 }
