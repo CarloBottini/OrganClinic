c678f27420d804e10915e0f3f5247ff5c2d9d4f5
CarloBottini <carlo.bottinimorcillo@usp.ceu.es>
2025-05-08 10:08:02 +0200

    modify operation JDBC

--- src/OrganClinicJDBC/JDBCOperationManager.java
+++ src/OrganClinicJDBC/JDBCOperationManager.java
@@ -6,6 +6,7 @@ import java.sql.PreparedStatement;
 import OrganClinicINTERFACEs.OperationManager;
 import OrganClinicPOJOs.Operation;
 import OrganClinicPOJOs.Organ;
+import OrganClinicPOJOs.Patient;
 
 public class JDBCOperationManager implements OperationManager{
 
@@ -37,8 +38,25 @@ private JDBCManager manager;
 	
 	
 	
-	
-	
+	  public void rescheduleOperation(Operation operation) {
+		try {
+			String query = "UPDATE Operation SET isDone= ?, date= ?, patient_id= ?, treatment_id= ?, doctor_id= ? WHERE id=?";
+			PreparedStatement prep = manager.getConnection().prepareStatement(query);
+			prep.setBoolean(1,operation.getIsDone());
+			prep.setDate(2,operation.getDate());
+			prep.setInt(3,operation.getPatient_id());
+			prep.setInt(4,operation.getTreatment_id());
+			prep.setInt(5,operation.getDoctor_id());
+			prep.setInt(6,operation.getId());
+
+	        prep.executeUpdate();
+	        prep.close();
+	        System.out.println("Operation with ID " + operation.getId() + " rescheduled successfully.");
+		}catch(Exception e){
+	        System.out.println("Error in the database");
+			e.printStackTrace();
+			}	
+	}
 	
 	
 	
