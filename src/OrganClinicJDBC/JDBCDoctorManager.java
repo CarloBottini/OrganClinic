2b85b29679c627dbab815c1e1517b2b56bc94de1
CarloBottini <carlo.bottinimorcillo@usp.ceu.es>
2025-05-09 15:31:46 +0200

    modify + delete doctor methods

--- src/OrganClinicJDBC/JDBCDoctorManager.java
+++ src/OrganClinicJDBC/JDBCDoctorManager.java
@@ -41,6 +41,7 @@ public class JDBCDoctorManager implements DoctorManager{
 			rs.close();
 			stmt.close();
 		}catch(Exception e){
+			System.out.println("Error in the database while getting all the doctors with their info");
 			e.printStackTrace();
 			}
 		return doctors;
@@ -59,7 +60,7 @@ public class JDBCDoctorManager implements DoctorManager{
 			prep.executeUpdate();
 			prep.close();
 		}catch(Exception e){
-			System.out.println("Error in the database");
+			System.out.println("Error in the database while adding a new doctor");
 			e.printStackTrace();
 		}
 	}
@@ -74,13 +75,11 @@ public class JDBCDoctorManager implements DoctorManager{
 			Doctor doc = new Doctor (rs.getInt("id"), rs.getString("name"), rs.getDate("dob"), rs.getString("gender"), rs.getString("email"), rs.getInt("telephone"));
 			return doc;
 		}catch (Exception e) {
-			System.out.println("Error in the database");
+			System.out.println("Error in the database while getting the doctor by ID");
 			e.printStackTrace();
 		}
 		return null;
 	}
-	
-	
 	
 	
 	public Doctor getDoctorByEmail(String email) {
@@ -94,11 +93,52 @@ public class JDBCDoctorManager implements DoctorManager{
 			prep.close();
 			return doc;
 		}catch (Exception e) {
-			System.out.println("Error in the database");
+			System.out.println("Error in the database while getting the doctor by email");
 			e.printStackTrace();
 		}
 		return null;
 	}
+	
+	
+	public void modifyDoctor(Doctor doc) {
+		try {
+			String query = "UPDATE Doctor SET name= ?, dob= ?, gender= ?, email= ?, telephone= ? WHERE id=?";
+			PreparedStatement prep = manager.getConnection().prepareStatement(query);
+			prep.setString(1,doc.getName());
+			prep.setDate(2, doc.getDob());
+			prep.setString(3, doc.getGender());
+	        prep.setString(4, doc.getEmail());
+	        prep.setInt(5, doc.getTelephone());
+	        prep.setInt(6, doc.getId());
+	        prep.executeUpdate();
+	        prep.close();
+	        System.out.println("Doctor with ID " + doc.getId() + " updated successfully.");
+	        
+		}catch(Exception e){
+	        System.out.println("Error in the database while updating the doctor");
+			e.printStackTrace();
+			}	
+	}
+	
+	
+	public void deletePatient (Integer id) {
+		try {
+			String st= "DELETE FROM Doctor WHERE id= ?";
+			PreparedStatement prep = manager.getConnection().prepareStatement(st);
+			prep.setInt(1, id);
+			int rowsAffected = prep.executeUpdate();
+			if (rowsAffected>0) {
+		        System.out.println("Deleted successfully the doctor with ID"+ id);
+			}else {
+		        System.out.println("Doctor not found with ID " + id);
+			}
+			prep.close();		
+		}catch(Exception e) {
+			System.out.println("Error while deleting the doctor in data bases with the doctor ID " + id);
+			e.printStackTrace();
+		}
+	}
+	
 	
 	
 }
